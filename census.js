15eea77c26997ef03b91345113ccd5527be643ff
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-03-26 00:15:47 -0400

    Added more data to csnsus.js

--- census.js
+++ census.js
@@ -5,7 +5,9 @@ const birdCensus = new HashMap(16);
 birdCensus.assign('mandarin duck', 'Central Park');
 birdCensus.assign('monk parakeet', 'Brooklyn');
 birdCensus.assign('horned owl', 'Manhattan');
+birdCensus.assign('peregrine falcon', 'Bronx');
 
 console.log(birdCensus.retrieve('mandarin duck'));
 birdCensus.print('monk parakeet');
 birdCensus.print('horned owl');
+birdCensus.print('peregrine falcon');
